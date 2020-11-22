8133a95192cf76db9fe5d338c252bf46e1eab66a
zambonil <lucaszam0@gmail.com>
2020-11-22 20:12:31 -0300

    Add layout responsiviness

--- script/script.js
+++ script/script.js
@@ -5,41 +5,13 @@ let minutes = time.getMinutes();
 let seconds = time.getSeconds();
 let htmlClock = document.getElementById('clock');
 let background = document.getElementById('time');
+let baseHexClock = '#' + hours + minutes + seconds;
+
+background.style.backgroundColor = baseHexClock;
+htmlClock.innerHTML = '#' + hours + ':' + minutes + ':' + seconds;
+
 let padToTwo = number => number <= 99 ? `0${number}`.slice(-2) : number;
-console.log('aaa')
 
-function timePass () {
-    if (seconds <= 60) {
-        seconds += 1;
-        console.log(seconds);
-    }
-    else if (minutes <= 60){
-        seconds = 00;
-        minutes += 1;
-    }
-    else if (hours <= 24) {
-        seconds = 00;
-        minutes = 00;
-        hours += 1;
-    }
-    else {
-        seconds = 00;
-        minutes = 00;
-        hours = 00;
-    }
-
-    let clockHours = padToTwo(hours);
-    let clockMinutes = padToTwo(minutes);
-    let clockSeconds = padToTwo(seconds);
-    let hexClock = '#' + clockHours + clockMinutes + clockSeconds;
-
-
-    htmlClock.innerHTML = '#' + clockHours + ':' + clockMinutes + ':' + padToTwo(seconds);
-    background.style.background = hexClock;
-
-}
-
-timePass();
 
 let timerId = setInterval(function () {
     if (seconds <= 60) {
@@ -64,7 +36,7 @@ let timerId = setInterval(function () {
     let clockHours = padToTwo(hours);
     let clockMinutes = padToTwo(minutes);
     let clockSeconds = padToTwo(seconds);
-    let hexClock = '#' + clockHours + clockMinutes + clockSeconds;
+    hexClock = '#' + clockHours + clockMinutes + clockSeconds;
 
 
     htmlClock.innerHTML = '#' + clockHours + ':' + clockMinutes + ':' + padToTwo(seconds);
