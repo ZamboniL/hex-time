3b4be3e4dfe8833e8deff33238cec7def15eca52
zambonil <lucaszam0@gmail.com>
2020-11-23 08:41:33 -0300

    Fix time overflow and clean code

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # hex-time
+
+A site that takes the current time value and transforms that into hexadecimal to use as background color, updating every second.
--- script/script.js
+++ script/script.js
@@ -1,48 +1,52 @@
 
+
 let time = new Date();
+// pad the given number to have at least two digis (1 to 01)
+let padToTwo = number => number <= 99 ? `0${number}`.slice(-2) : number;
+
 let hours = time.getHours();
 let minutes = time.getMinutes();
 let seconds = time.getSeconds();
-let htmlClock = document.getElementById('clock');
-let background = document.getElementById('time');
-let baseHexClock = '#' + hours + minutes + seconds;
-
-background.style.backgroundColor = baseHexClock;
-htmlClock.innerHTML = '#' + hours + ':' + minutes + ':' + seconds;
-
-let padToTwo = number => number <= 99 ? `0${number}`.slice(-2) : number;
 
 
-let timerId = setInterval(function () {
-    if (seconds <= 60) {
+let clock = document.getElementById('clock');
+let container = document.getElementById('container');
+
+updateClock(hours, minutes, seconds);
+
+// Update the passage of time then 
+// calls the updateClock function
+let timer = setInterval(function () {
+    if (seconds < 59) {
         seconds += 1;
-        console.log(seconds);
     }
-    else if (minutes <= 60){
-        seconds = 00;
+    else if (minutes < 59){
+        seconds = 0;
         minutes += 1;
     }
-    else if (hours <= 24) {
-        seconds = 00;
-        minutes = 00;
+    else if (hours < 23) {
+        seconds = 0;
+        minutes = 0;
         hours += 1;
     }
     else {
-        seconds = 00;
-        minutes = 00;
-        hours = 00;
+        seconds = 0;
+        minutes = 0;
+        hours = 0;
     }
 
-    let clockHours = padToTwo(hours);
-    let clockMinutes = padToTwo(minutes);
-    let clockSeconds = padToTwo(seconds);
-    hexClock = '#' + clockHours + clockMinutes + clockSeconds;
-
-
-    htmlClock.innerHTML = '#' + clockHours + ':' + clockMinutes + ':' + padToTwo(seconds);
-    background.style.background = hexClock;
+    updateClock(hours, minutes, seconds)
 
 }, 1000);
 
+// Update background color and live clock
+// with the current time
+function updateClock(hours, minutes, seconds) {
 
+    hours = padToTwo(hours);
+    minutes = padToTwo(minutes);
+    seconds = padToTwo(seconds);
 
+    clock.innerHTML = '#' + hours + ':' + minutes + ':' + seconds;
+    container.style.background = '#' + hours + minutes + seconds;
+}
